3acdc4d9b5bae79d3baebb3f180725629c97c492
ShuangYa <896640547@qq.com>
2019-05-10 11:10:56 +0800

    Update docs

--- app/Configuration.php
+++ app/Configuration.php
@@ -16,10 +16,11 @@ use Psr\SimpleCache\CacheInterface;
 class Configuration {
 	const ROUTE_VER = 1;
 	public function setRouter(CacheInterface $cache) {
-		if ($cache->has('routes') && $cache->get('routes_ver') == self::ROUTE_VER) {
-			Router::from($cache->get('routes'));
-			return;
-		}
+		// If you want to cache defined routes
+		// if ($cache->has('routes') && $cache->get('routes_ver') == self::ROUTE_VER) {
+		// 	Router::from($cache->get('routes'));
+		// 	return;
+		// }
 		Router::get('/api/user/me', 'api.user.me');
 		Router::post('/api/user/login', 'api.user.login');
 		Router::post('/api/user/register', 'api.user.register');
@@ -29,7 +30,7 @@ class Configuration {
 		Router::get('/register', 'index.index.registerPage');
 		Router::post('/register', 'index.index.register');
 		Router::get('/', 'index.index.index');
-		$cache->set('routes', Router::dump());
-		$cache->set('routes_ver', self::ROUTE_VER);
+		// $cache->set('routes', Router::dump());
+		// $cache->set('routes_ver', self::ROUTE_VER);
 	}
 }
